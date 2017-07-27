0b4d870fcaa75bf345cef99bab7729de51f7a9f0
codemauri <semjase77@gmail.com>
2017-07-27 14:30:09 -0500

    Trailing spaces

--- core/src/main/java/pro/jtaylor/timetracker/core/dao/package-info.java
+++ core/src/main/java/pro/jtaylor/timetracker/core/dao/package-info.java
@@ -1,10 +1,10 @@
 /**
- * Provides the classes necessary to create an applet and the classes an applet uses 
- * to communicate with its applet context. 
+ * Provides the classes necessary to create an applet and the classes an applet uses
+ * to communicate with its applet context.
  * <p>
- * The applet framework involves two entities: 
- * the applet and the applet context. An applet is an embeddable window (see the 
- * class) with a few extra methods that the applet context 
+ * The applet framework involves two entities:
+ * the applet and the applet context. An applet is an embeddable window (see the
+ * class) with a few extra methods that the applet context
  * can use to initialize, start, and stop the applet.
  *
  * @since 1.0
